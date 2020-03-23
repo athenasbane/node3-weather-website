74042ade6f05034b7c6007043e180814e90ce969
Benjamin Collingwood <benjamin.collingwood@artemis.works>
2020-03-23 16:43:44 +0000

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'https://api.darksky.net/forecast/b6efeb00ceb5b45b61d6b6ad4d6350f3/' + latitude + ',' + longitude + '?units=uk2' 
+
+    request({url, json:true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services')
+        } else if (body.error) {
+            callback('bad coordinates given')
+        
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
